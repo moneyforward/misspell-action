3b8d29874109db716833ac575d4dd525439ccff5
Naoki Kimura <n.kimura.cap@gmail.com>
2020-04-27 17:15:47 +0900

    Fix a bug

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import Analyzer from './analyzer';
 
 (async (): Promise<void> => {
   const files = (process.env.INPUT_FILES || '.');
-  const locale = process.env.INPUT_LOCALE;
+  const locale = process.env.INPUT_LOCALE || 'US';
   if (!(locale === undefined || (locale === 'UK' || locale === 'US')))
     throw new TypeError('Please specify either US or UK for the locale.');
   const ignore = process.env.INPUT_IGNORE;
