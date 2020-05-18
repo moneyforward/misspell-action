483995f63588b0ac8b386faf040986fbfb20f26c
Naoki Kimura <n.kimura.cap@gmail.com>
2020-05-18 17:43:17 +0900

    Fix a bug

--- src/action.ts
+++ src/action.ts
@@ -1,11 +1,9 @@
 import CodeReviewAction from '@moneyforward/code-review-action';
-import Analyzer, { isLocale } from '.';
+import Analyzer from '.';
 
 (async (): Promise<void> => {
   console.log('::echo::%s', process.env['RUNNER_DEBUG'] === '1' ? 'on' : 'off');
   try {
-    if (!(process.env.INPUT_LOCALE === undefined || isLocale(process.env.INPUT_LOCALE)))
-      throw new TypeError('Please specify either US or UK for the locale.');
     const locale = process.env.INPUT_LOCALE;
     const ignore = process.env.INPUT_IGNORE;
     process.env['INPUT_OPTIONS'] = JSON.stringify([locale, ignore]);
